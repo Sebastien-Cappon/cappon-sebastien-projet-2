51d63276cae5bbcec2660a4545208b5636c148d4
Sébastien Cappon <contact.sae.echo@gmail.com>
2023-03-27 11:58:36 +0200

    main implementation

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -3,4 +3,20 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.Map;
 
-// WRITE YOUR CODE HERE
+public class Main {
+
+  public static void main(String[] args) {
+    List<String> rawSymptomsList;
+    Map<String, Integer> countedSymptomsList;
+    Map<String, Integer> orderedSymptomsList;
+
+    ISymptomReader readSymptoms = new ReadSymptomDataFromFile("symptoms.txt");
+    ISymptomWriter writeSymptoms = new WriteSymptomDataToFile();
+    AnalyticsCounter analyticsCounter = new AnalyticsCounter(readSymptoms, writeSymptoms);
+
+    rawSymptomsList = analyticsCounter.getSymptoms();
+    countedSymptomsList = analyticsCounter.countSymptoms(rawSymptomsList);
+    orderedSymptomsList = analyticsCounter.sortSymptoms(countedSymptomsList);
+    analyticsCounter.writeSymptoms(orderedSymptomsList);
+  }
+}
