974080565852c5592b169c4a4c8cb04cf8ca0d21
Sébastien Cappon <contact.sae.echo@gmail.com>
2023-03-27 11:55:39 +0200

    writer implementation

--- com/hemebiotech/analytics/ISymptomWriter.java
+++ com/hemebiotech/analytics/ISymptomWriter.java
@@ -2,11 +2,6 @@ package com.hemebiotech.analytics;
 
 import java.util.Map;
 
-
-/**
- * Anything that will write symptom data on a valid data output format
- *
- */
 public interface ISymptomWriter {
-    void writeSymptoms(Map<String, Integer> symptoms);
+  public void writeSymptoms(Map<String, Integer> symptoms);
 }
--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,14 +1,27 @@
 package com.hemebiotech.analytics;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
-  private String filepathTo;
-	
-	public WriteSymptomDataToFile (String filepathTo) {
-		this.filepathTo = filepathTo;
-	}
+  
+  @Override
+  public void writeSymptoms(Map<String, Integer> symptoms) {
+    try {
+      FileWriter fileWriter = new FileWriter("result.out", false);
+      BufferedWriter writer = new BufferedWriter(fileWriter);
+      
+      for (Map.Entry<String, Integer> symptom : symptoms.entrySet()) {
+        writer.write(symptom.getKey() + " : " + symptom.getValue());
+        writer.newLine();
+      }
+
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
