8314ee80f606c17641f6cfe5c8804ad27dd05415
Sébastien Cappon <contact.sae.echo@gmail.com>
2023-03-27 11:57:18 +0200

    refactor analytics counter

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,40 +1,47 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception {
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+  ISymptomReader reader;
+  ISymptomWriter writer;
 
-		int i = 0;
-		int headCount = 0;
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+  public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+    this.reader = reader;
+    this.writer = writer;
+  }
 
-			line = reader.readLine();
-		}
-		
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+  public List<String> getSymptoms() {
+    return reader.getSymptoms();
+  }
+  
+  public void writeSymptoms(Map<String, Integer> symptoms) {
+    writer.writeSymptoms(symptoms); 
+  }
+
+  public Map<String, Integer> countSymptoms(List<String> symptoms) {
+    Map<String, Integer> symptomsOccurencies = new HashMap<String, Integer>();
+    
+    for (int i = 0; i < symptoms.size(); i++) {
+      if (symptomsOccurencies.containsKey(symptoms.get(i))) {
+        symptomsOccurencies.put(symptoms.get(i), (symptomsOccurencies.get(symptoms.get(i)) + 1));
+      } else {
+        symptomsOccurencies.put(symptoms.get(i), 1);
+      }
+    }
+
+    return symptomsOccurencies;
+  }
+  
+  public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+    Map<String, Integer> sortedSymptoms = new TreeMap<String, Integer>();
+
+    sortedSymptoms.putAll(symptoms);
+
+    return sortedSymptoms;
+  }
 }
